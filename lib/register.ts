81e0eba58ac1910fb77934a881010bfcd30f50ce
CrSjimo <crs_16423@outlook.com>
2019-07-29 21:23:34 +0800

    add bugs

--- lib/register.ts
+++ lib/register.ts
@@ -22,10 +22,12 @@ server.route({
     method:'POST',
     path:'/register',
     handler:(request,h) => {
+        console.log(`notice[new request]: ${request.info.id}`);
         const {payload} = (request as {payload:UnValidatedRegisterRequest});
         if(payload.username && payload.passwordSHA256 && payload.inviteCode){
             if(!(user.username.regexp.test(payload.username)))
             if(queryUserViaUsername(payload.username)){
+                console.log(`notice[err status]: user ${payload.username} already exists`)
                 h.response({
                     status:'User Already Registered',
                     userid:-1,
@@ -41,12 +43,14 @@ server.route({
                     })
                     throw err;
                 }
+                console.log(`notice[new user]: ${payload.username}`);
                 h.response({
                     status:'Success',
                     userid:res.insertID,
                 });
             });
         }else{
+            console.log(`notice[err status]: invalid value by user ${payload.username}`);
             h.response({
                 status:'Invalid',
                 userid:-1,
@@ -55,21 +59,34 @@ server.route({
         return '';
     }
 });
-const init = async () => {
-    await server.start();
-    console.log(`Server running at ${server.info.uri}`)
+server.route({
+    method:'GET',
+    path:'/register',
+    handler:(request,h)=>{
+        return h.file('./account-client/test/testpage.html');
+    }
+})
+async function init (){
     mysqlConnection.connect((err)=>{
         if(err){
             throw err;
         }
     });
     console.log(`mysql connected.`)
+    await server.start();
+    await server.register(inert);
+    console.log(`Server running at ${server.info.uri}`)
+}
+async function stop(){
+    mysqlConnection.end();
+    console.log('MySQL stops.');
+    await server.stop();
+    console.log('Server stops.');
 }
 init();
 process.on('exit',() => {
-    server.stop();
-    console.log('Server stops.');
-    mysqlConnection.end();
-    console.log('MySQL stops.');
-
-})
+    stop();
+});
+// process.on('SIGINT',()=>{
+//     stop();
+// })
