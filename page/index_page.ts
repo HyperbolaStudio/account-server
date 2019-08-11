590fbfd73663c8aea7a0b1c91bdd0ef17e017b0f
CrSjimo <crs_16423@outlook.com>
2019-08-11 20:40:44 +0800

    add bugs

--- page/index_page.ts
+++ page/index_page.ts
@@ -1,37 +1,29 @@
 import ejs from 'ejs'
 import { server } from '../lib/server_init';
-import { readFile } from 'fs';
 import { querySession } from '../api_utils/session_utils';
 import { queryUserViaUserID } from '../api_utils/user_queries';
 server.route({
     method:'GET',
     path:'/',
     handler:async (request,h)=>{
-        console.log(1);
         let isLoggedIn = true;
         const session = request.state.session;
         let userid:number|null = 0;
         let username = '';
         try{
-            console.log(2);
             if(!session || !session.sessionID || !(userid = await querySession(session.sessionID))){
                 isLoggedIn = false;
             }else{
-                console.log(3);
                 let u = await queryUserViaUserID(userid)
-                console.log(4);
                 if(u){
                     username = u.username;
                 }else{
-                    console.log(5);
                     throw new Error();
                 }
             }
         }catch(e){
-            console.log(6);
             isLoggedIn = false;
         }
-        console.log(7);
         let res = '';
         try{
             res = await ejs.renderFile('./account-client/view/index.ejs',{
@@ -64,9 +56,9 @@ server.route({
             });
         }catch(e){
             console.log(e);
+            return h.response().code(500);
         }
-        
-        console.log(res);
+        // console.log(res);
         return res;
     }
 })
